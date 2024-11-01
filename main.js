9f23130acd8b570c1c40cd8a5914c2e7b298e717
Felipe Veras <l.felipeveras@gmail.com>
2024-11-01 11:13:15 -0300

    Adding the infra structure to ipc communication between the renderer thread and the background thread and the http communication with ollama using axios

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-import { app, BrowserWindow } from 'electron';
+import { app, BrowserWindow, ipcMain } from 'electron';
 import path from 'node:path';
 import { fileURLToPath } from 'url';
+import axios from 'axios';
 
 // circuvent the absence of __dirname
 const __filename = fileURLToPath(import.meta.url);
@@ -36,3 +37,32 @@ const createWindow = () => {
   app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
   })
+
+  // Inter proccess communication
+
+  ipcMain.on('query-ollama', (event, query) => {
+    callLLM(query)
+  })
+
+  // General functions
+
+  const callLLM = async (query) => {
+    var querySuccess = true;
+    
+    const response = await axios.post("http://localhost:11434/api/generate", {
+      model: "llama3.2",
+      prompt: query
+    }, {
+      responseType: 'stream'
+    }).catch(function (error){
+      querySuccess = false;
+    })
+
+    if(querySuccess) {
+      const stream = response.data
+      stream.on('data', data => {
+        data = data.toString()
+        //TODO send back to renderer thread
+      })
+    }
+  }
